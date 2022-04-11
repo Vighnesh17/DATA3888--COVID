4f2870e67284e51dc5011df3a0c7630650f4c034
Diana Cao <zcao8597@uni.sydney.edu.au>
2022-04-11 10:01:58 +1000

    Added iso code dataset

--- covidApp/global.R
+++ covidApp/global.R
@@ -1,8 +1,12 @@
 library(tidyverse)
 library(lubridate)
 
-## load in data, enable if there is need to initiate covid_data df
+## load in covid data, enable if there is need to initiate covid_data df
 # covid_data <- read.csv("https://covid.ourworldindata.org/data/owid-covid-data.csv", stringsAsFactors = FALSE, check.names =  FALSE)
+
+## load in iso data, enable if there is need to initiate iso_data df
+isourl = "https://gist.githubusercontent.com/metal3d/5b925077e66194551df949de64e910f6/raw/c5f20a037409d96958553e2eb6b8251265c6fd63/country-coord.csv"
+iso_data = read.csv(isourl, sep = ",", strip.white = TRUE)
 
 covid_data = covid_data %>% 
   mutate(
